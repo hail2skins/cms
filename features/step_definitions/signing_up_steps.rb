3e7b3869f312527d1d8ecc4433218c7c790407b5
Art Mills <art@hamcois.com>
2014-01-28 21:26:25 +0000

    Just a wee more before stopping.

--- features/step_definitions/signing_up_steps.rb
+++ features/step_definitions/signing_up_steps.rb
@@ -3,11 +3,11 @@ Given(/^I am at the home page of the site$/) do
 end
 
 When(/^I click the link to sign up$/) do
-  pending # express the regexp above with the code you wish you had
+  click_link "Sign up now!"
 end
 
 Then(/^I should be able to view a sign up form$/) do
-  pending # express the regexp above with the code you wish you had
+  find('h1').should have_content("Sign up")
 end
 
 Then(/^fill in my information$/) do
