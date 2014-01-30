0ba020752eeabb846fc63a5e6a11904c89608089
Art Mills <art@hamcois.com>
2014-01-30 04:18:50 +0000

    Expanding first feature.

--- features/step_definitions/signing_up_steps.rb
+++ features/step_definitions/signing_up_steps.rb
@@ -11,5 +11,28 @@ Then(/^I should be on the sign up page$/) do
 end
 
 Then(/^be able to fill in my information$/) do
+  fill_in('First name', with: 'Art')
+  fill_in('Middle name', with: '')
+	fill_in('Last name', with: 'Mills')
+	fill_in('Email', with: 'test@test.com')
+	fill_in('Password', with: 'password')
+	fill_in('Confirmation', with: 'password')
+	should have_content("Please provide an address for you.  We'll get your business address later in the process:")
+	fill_in('Address 1', with: '1127 S. Trappers Crossing')
+	fill_in('Address 2', with: '')
+	fill_in('City', with: 'Hugo')
+	fill_in('State', with: 'MN')
+	fill_in('Zip', with: '55038')
+end
+
+Then(/^when I submit my information$/) do
+  click_button('Create Owner')
+end
+
+Then(/^I should be an owner created successfully$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be taken to the owner's home page$/) do
   pending # express the regexp above with the code you wish you had
 end
