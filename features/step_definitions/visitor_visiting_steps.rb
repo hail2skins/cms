4258bc04225f7e69b0cbc3476c1387c02859690a
Art Mills <art@hamcois.com>
2014-02-02 01:40:17 +0000

    Starting second feature test.

---
+++ features/step_definitions/visitor_visiting_steps.rb
@@ -0,0 +1,23 @@
+Given(/^I am at the home page of the site as a guest$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I can see the site is in alpha testing$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I can see a link to sign up$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I can see links to help, about and contact$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click help from the home page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see the help page from the home page$/) do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/visitor_visiting.feature
@@ -0,0 +1,14 @@
+Feature: Visitor Visiting
+	In order to see the home page of the Hamco Internet Solutions CMS page
+	As a visitor who has not signed up
+	I should see a default landing page with basic information about the site
+	that allows me to sign up.
+
+	Scenario: Visiting the home page
+		Given I am at the home page of the site as a guest
+		And I can see the site is in alpha testing
+		And I can see a link to sign up
+		And I can see links to help, about and contact
+		When I click help from the home page
+		Then I see the help page from the home page
+		
