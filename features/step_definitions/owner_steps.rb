be736ec5c35a67b0e00fc581d8c449f54298eb89
Art Mills <art@hamcois.com>
2014-02-06 17:42:32 +0000

    Owner steps added.

---
+++ features/step_definitions/owner_steps.rb
@@ -0,0 +1,23 @@
+Given(/^I am successfully signed up and signed in$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I am on my owner profile page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be told I should create a business$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a link to create a business$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^when I click the link to create a business$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be on the new business page$/) do
+  pending # express the regexp above with the code you wish you had
+end
